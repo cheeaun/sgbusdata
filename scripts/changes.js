3104ed970e93facf80ef463b099cfdaace7542be
Lim Chee Aun <cheeaun@gmail.com>
2021-04-28 22:31:11 +0800

    Split the "Stops changed" into 2: "Stop names changed" & "Stop locations changed"
    Also introduce nlog and log

--- scripts/changes.js
+++ scripts/changes.js
@@ -11,10 +11,11 @@ const readOldNewData = (path) => {
   return [oldData, newData];
 };
 
-const log = (str) => console.log('\n' + str);
+const nlog = (str) => console.log(`\n${str}`);
+const log = (str) => console.log(str);
 
 // It's fine to use SG timezone here because everything is in SG
-console.log(
+log(
   `# ${new Date().toLocaleDateString('en-SG', {
     year: 'numeric',
     month: 'long',
@@ -28,7 +29,7 @@ const oldStopsCount = Object.keys(oldStops).length;
 const newStopsCount = Object.keys(newStops).length;
 
 if (oldStopsCount !== newStopsCount) {
-  log(`## Stops count change: ${oldStopsCount} ⮕ ${newStopsCount}`);
+  nlog(`## Stops count change: ${oldStopsCount} ⮕ ${newStopsCount}`);
 }
 
 const stopsDiff = diff(oldStops, newStops);
@@ -36,40 +37,40 @@ const stopsDiff = diff(oldStops, newStops);
 if (stopsDiff.length) {
   const addedDiff = stopsDiff.filter((d) => d.op === 'add');
   if (addedDiff.length) {
-    log(`### Stops added: ${addedDiff.length}\n`);
+    nlog(`### Stops added: ${addedDiff.length}\n`);
     addedDiff.forEach((d) => {
       const { path, value } = d;
-      console.log(`- \`${path[0]}\` ${value[2]}`);
+      log(`- \`${path[0]}\` ${value[2]}`);
     });
   }
 
   const removedDiff = stopsDiff.filter((d) => d.op === 'remove');
   if (removedDiff.length) {
-    log(`### Stops removed: ${removedDiff.length}\n`);
+    nlog(`### Stops removed: ${removedDiff.length}\n`);
     removedDiff.forEach((d) => {
       const { path, value } = d;
-      console.log(`- \`${path[0]}\` ${value[2]}`);
+      log(`- \`${path[0]}\` ${value[2]}`);
     });
   }
 
   const replacedDiff = stopsDiff.filter((d) => d.op === 'replace');
   if (replacedDiff.length) {
-    log(`### Stops changed: ${replacedDiff.length}\n`);
-    replacedDiff.forEach((d) => {
-      const { path, value } = d;
-      const number = path[0];
-      const changeStatus = path[1] === 2 ? 'name' : 'location';
-      if (changeStatus === 'name') {
-        const oldValue = oldStops[path[0]][path[1]];
-        console.log(`- \`${number}\` ${oldValue} ⮕ ${value}`);
-      } else {
-        // location
-        const oldCoord = [oldStops[path[0]][0], oldStops[path[0]][1]];
-        const newCoord = [newStops[path[0]][0], newStops[path[0]][1]];
-        console.log(
-          `- \`${number}\` ${oldCoord.join(',')} ⮕ ${newCoord.join(',')}`,
-        );
-      }
+    const nameChangedDiff = replacedDiff.filter((d) => d.path[1] === 2);
+    const locationChangedDiff = replacedDiff.filter((d) => d.path[1] !== 2);
+
+    nlog(`### Stop names changed: ${nameChangedDiff.length}\n`);
+    nameChangedDiff.forEach((d) => {
+      const { value } = d;
+      const oldValue = oldStops[path[0]][path[1]];
+      log(`- \`${number}\` ${oldValue} ⮕ ${value}`);
+    });
+
+    nlog(`### Stop locations changed: ${locationChangedDiff.length}\n`);
+    locationChangedDiff.forEach((d) => {
+      const { path } = d;
+      const oldCoord = [oldStops[path[0]][0], oldStops[path[0]][1]];
+      const newCoord = [newStops[path[0]][0], newStops[path[0]][1]];
+      log(`- \`${number}\` ${oldCoord.join(',')} ⮕ ${newCoord.join(',')}`);
     });
   }
 }
@@ -80,7 +81,7 @@ const oldServicesCount = Object.keys(oldServices).length;
 const newServicesCount = Object.keys(newServices).length;
 
 if (oldServicesCount !== newServicesCount) {
-  log(`## Services count change: ${oldServicesCount} ⮕ ${newServicesCount}`);
+  nlog(`## Services count change: ${oldServicesCount} ⮕ ${newServicesCount}`);
 }
 
 const servicesDiff = diff(oldServices, newServices);
@@ -90,10 +91,10 @@ if (servicesDiff.length) {
     (d) => d.op === 'add' && d.path[1] !== 'routes',
   );
   if (addedDiff.length) {
-    log(`### Services added: ${addedDiff.length}\n`);
+    nlog(`### Services added: ${addedDiff.length}\n`);
     addedDiff.forEach((d) => {
       const { path, value } = d;
-      console.log(`- \`${path[0]}\` ${value.name}`);
+      log(`- \`${path[0]}\` ${value.name}`);
     });
   }
 
@@ -101,11 +102,11 @@ if (servicesDiff.length) {
     (d) => d.op === 'remove' && d.path[1] !== 'routes',
   );
   if (removedDiff.length) {
-    log(`### Services removed: ${removedDiff.length}\n`);
+    nlog(`### Services removed: ${removedDiff.length}\n`);
     removedDiff.forEach((d) => {
       const { path } = d;
       const name = oldServices[path[0]].name;
-      console.log(`- \`${path[0]}\` ${name}`);
+      log(`- \`${path[0]}\` ${name}`);
     });
   }
 
@@ -116,7 +117,7 @@ if (servicesDiff.length) {
   });
 
   if (changedRoutesServices.size) {
-    log(`### Bus Stop Changes To Routes: ${changedRoutesServices.size}\n`);
+    nlog(`### Bus Stop Changes To Routes: ${changedRoutesServices.size}\n`);
     changedRoutesServices.forEach((s) => {
       const oldRoutes = oldServices[s].routes;
       const newRoutes = newServices[s].routes;
@@ -124,7 +125,7 @@ if (servicesDiff.length) {
       const { added, removed } = hyperdiff(oldRoutes.flat(), newRoutes.flat());
       const addedCount = added.length;
       const removedCount = removed.length;
-      console.log(
+      log(
         `- \`${s}\` ${newServices[s].name}: ${
           addedCount ? `+${addedCount}` : ''
         }${addedCount && removedCount ? ', ' : ''}${
